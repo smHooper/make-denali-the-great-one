733ea30c2bf4edd552f900b736c1e0dcecb6dcb0
sam hooper <samhooperstudio@gmail.com>
2025-04-17 01:28:33 -0800

    when window scrolls past hero, make nav backcound color visible

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-function toggleNavbarVisibility(e) {
-    const $navbarToggle = $('#navbar-toggle');
-    $navbarToggle.attr("aria-expanded", !($navbarToggle.attr("aria-expanded") === 'true'));
-};
-
 $(document).ready(() => {
+    const $navbarToggle = $('#navbar-toggle');
+
+    function toggleNavbarVisibility(e) {
+        $navbarToggle.attr('aria-expanded', !($navbarToggle.attr("aria-expanded") === 'true'));
+    };
+
     // toggle mobile nav bar links
-    $('#navbar-toggle').click(toggleNavbarVisibility);
+    $('#navbar-toggle, .navbar-link').click(toggleNavbarVisibility);
     
     // hide the nav bar when a user clicks a link
     $('#navbar-menu').click(toggleNavbarVisibility);
 
     // prevent link click events from bubbling
     $('.navbar-link-list').click(e => e.stopPropagation());
+
+    // when the window is scrolled past the hero section
+    //  set the background color of the nav to not be transparent
+    function toggleNavBackground() {
+      var scrollPosition = window.scrollY;
+      var heroHeight = $('section.hero').height();
+      // .scrolled class sets BG color
+      $('#navbar').toggleClass('scrolled', scrollPosition >= heroHeight);
+    }
+
+    $(window).on('scroll resize', toggleNavBackground);
+    toggleNavBackground();
+
 });
